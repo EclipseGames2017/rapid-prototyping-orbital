3181e62121ed36e5ff97cd7186fca7f5cddbee33
Jamie M Sams <sams_drum@hotmail.com>
2017-12-04 19:49:28 +0000

    - Added a 'how to play' screen - Added a version label - Added a feedback button that links to a google form

---
+++ Assets/Scripts/FeedbackButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EclipseStudios.Orbital
+{
+    public class FeedbackButton : MonoBehaviour
+    {
+        public void OpenFeedbackForm()
+        {
+            Application.OpenURL("https://goo.gl/forms/JrTFjfilNNWTngoP2");
+        }
+    }
+}

12be6e44f698b6f8ed06fee0ad8794caaa8a43c6
Jamie <sams_drum@hotmail.com>
2017-12-07 15:38:00 +0000

    Started work on the ball death effect

---
+++ Assets/Scripts/Colour Switchers/ParticleColourLoader.cs
@@ -0,0 +1,45 @@
+﻿using FallingSloth;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EclipseStudios.Orbital
+{
+    public class ParticleColourLoader : MonoBehaviour
+    {
+        public static List<ParticleColourLoader> activeInstances;
+
+        ParticleSystem particles;
+
+        public Gradient defaultGradient;
+
+        void Start()
+        {
+            particles = GetComponent<ParticleSystem>();
+            UpdateColour(SaveDataManager<OrbitalSaveData>.data.colour);
+        }
+
+        void OnEnable()
+        {
+            if (activeInstances == null)
+                activeInstances = new List<ParticleColourLoader>();
+            activeInstances.Add(this);
+            particles.enableEmission = false;
+        }
+        void OnDisable()
+        {
+            activeInstances.Remove(this);
+        }
+
+        public void UpdateColour(Color colour)
+        {
+            System.Array.ForEach(defaultGradient.colorKeys, (gc) => {
+                gc.color *= colour;
+                Debug.Log("Colour set to: " + gc.color);
+            });
+
+            ParticleSystem.MainModule main = particles.main;
+            main.startColor = defaultGradient;
+        }
+    }
+}
